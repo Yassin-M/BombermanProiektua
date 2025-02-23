a517c658fcfeaa872c097f8451c915af396349e3
ander <ander@Ander-Laptop-Ubuntu>
2025-02-23 11:38:21 +0100

    Aldatu bista JFrame izateko

--- BombermanProiektua/src/Bista/IkusiBeharrekoa.java
+++ BombermanProiektua/src/Bista/IkusiBeharrekoa.java
@@ -1,5 +1,36 @@
 package Bista;
 
-public class IkusiBeharrekoa {
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class IkusiBeharrekoa extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					IkusiBeharrekoa frame = new IkusiBeharrekoa();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public IkusiBeharrekoa() {
+		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
 
 }
