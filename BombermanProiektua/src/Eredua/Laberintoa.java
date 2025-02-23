57112560187301bfe30bffe5f132747c3f10862a
Unaire123 <unair163@gmail.com>
2025-02-23 11:21:35 +0100

    Laberintoaren Singleton-a

--- BombermanProiektua/src/Eredua/Laberintoa.java
+++ BombermanProiektua/src/Eredua/Laberintoa.java
@@ -1,5 +1,17 @@
 package Eredua;
 
 public class Laberintoa {
-
+	private static Laberintoa nireLaberintoa;
+	private Gelaxka[][] laberintoa;
+	
+	private Laberintoa() {
+		this.laberintoa = new Gelaxka[11][17];
+	}
+	
+	public static Laberintoa getNireLaberintoa() {
+		if (nireLaberintoa == null) {
+			nireLaberintoa = new Laberintoa();
+		}
+		return nireLaberintoa;
+	}
 }
