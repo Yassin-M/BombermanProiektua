a89bcc51d5f786ba8ae3cb37d73a5f5f5a1bf37a
urkibiris <urkibiris@vivobook-s>
2025-02-25 17:51:23 +0100

    gelaxka klasea

---
+++ BombermanProiektua/src/Eredua/BlokeMota.java
@@ -0,0 +1,10 @@
+package Eredua;
+
+public enum BlokeMota {
+	SUA,
+	BOMBA,
+	BLOKEBIGUNA,
+	BLOKEGOGORRA,
+	ETSAIA,
+	JOKALARIA
+}
--- BombermanProiektua/src/Eredua/Gelaxka.java
+++ BombermanProiektua/src/Eredua/Gelaxka.java
@@ -1,5 +1,69 @@
 package Eredua;
 
-public class Gelaxka {
+import java.util.Observable;
 
+public class Gelaxka extends Observable {
+	private Sua sua;
+	private Bomba bomba;
+	private BlokeBiguna blokeBiguna;
+	private BlokeGogorra blokeGogorra;
+	private Etsaia etsaia;
+	private Jokalaria jokalari;
+	private BlokeMota blokeMota;
+	
+	
+	public Gelaxka() {
+		this.sua = null;
+        this.bomba = null;
+        this.blokeBiguna = null;
+        this.blokeGogorra = null;
+        this.etsaia = null;
+        this.jokalari = null;
+        this.blokeMota = null;
+	}
+	
+	public void setSua() {
+		this.sua = new Sua();
+		kenduAurrekoa();
+		this.blokeMota = BlokeMota.SUA;
+		setChanged();
+		notifyObservers();
+	}
+	public void setBlokeBiguna() {
+		this.blokeBiguna = new BlokeBiguna();
+		kenduAurrekoa();
+		this.blokeMota = BlokeMota.SUA;
+		setChanged();
+		notifyObservers();
+	}
+	public void setBlokeGogorra() {
+		this.blokeGogorra = new BlokeGogorra();
+		kenduAurrekoa();
+		this.blokeMota = BlokeMota.SUA;
+		setChanged();
+		notifyObservers();
+	}
+	
+	private void kenduAurrekoa() {
+		switch (this.blokeMota) {
+			case SUA:
+                this.sua = null;
+                break;
+            case BOMBA:
+            	this.bomba = null;
+                break;
+            case BLOKEBIGUNA:
+            	this.blokeBiguna = null;
+				break;
+			case BLOKEGOGORRA:
+				this.blokeGogorra = null;
+				break;
+			case ETSAIA:
+				this.etsaia = null;
+				break;
+			case JOKALARIA:
+				this.jokalari = null;
+				break;
+		}
+	}
 }
