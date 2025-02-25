0f4766f649084ec87e0aa58fbb46200a0596aa79
ander <ander@Ander-Laptop-Ubuntu>
2025-02-25 18:02:47 +0100

    Laberintoa sortzeko metodoa

--- BombermanProiektua/src/Eredua/Laberintoa.java
+++ BombermanProiektua/src/Eredua/Laberintoa.java
@@ -1,11 +1,15 @@
 package Eredua;
 
-public class Laberintoa {
+import java.util.Observable;
+import java.util.Random;
+@SuppressWarnings("deprecation")
+
+public class Laberintoa extends Observable{
 	private static Laberintoa nireLaberintoa;
 	private Gelaxka[][] laberintoa;
 	
 	private Laberintoa() {
-		this.laberintoa = new Gelaxka[11][17];
+		laberintoaSortu();
 	}
 	
 	public static Laberintoa getNireLaberintoa() {
@@ -13,5 +17,36 @@ public class Laberintoa {
 			nireLaberintoa = new Laberintoa();
 		}
 		return nireLaberintoa;
+	}
+	
+	private void laberintoaSortu() {
+		this.laberintoa = new Gelaxka[11][17];
+		for (int i=0; i<11; i++) {
+			for (int j=0; j<17; j++) {
+				if (i==0 && j==0) {
+					//laberintoa[i][j] = new Gelaxka().setJokalaria();
+				}
+				else if ((i==0 && j==1)||(i==1 && j==0)) {
+					laberintoa[i][j] = new Gelaxka();
+				}
+				else if (i%2 != 0 && i%2 != 0) {
+					//laberintoa[i][j] = new Gelaxka().setBlokeGogorra();
+				}
+				else {
+					Random r = new Random();
+					if (r.nextInt(100) >= 40) {
+						//laberintoa[i][j] = new Gelaxka().setBlokeBiguna();
+					}
+					else if (r.nextInt(100) >= 90) {
+						//laberintoa[i][j] = new Gelaxka().setEtsaia();
+					}
+					else {
+						laberintoa[i][j] = new Gelaxka();
+					}
+				}
+			}
+		}
+		setChanged();
+		notifyObservers();
 	}
 }
