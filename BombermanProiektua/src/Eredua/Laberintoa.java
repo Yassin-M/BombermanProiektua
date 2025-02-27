fbdea1f224d981679222a0c03f0b958b84a58501
anderibarraa <aibarra039@ikasle.ehu.eus>
2025-02-27 11:12:45 +0100

    zuzendu bloke gogorren errorea

--- BombermanProiektua/src/Eredua/Laberintoa.java
+++ BombermanProiektua/src/Eredua/Laberintoa.java
@@ -30,7 +30,7 @@ public class Laberintoa extends Observable{
 				else if ((i==0 && j==1)||(i==1 && j==0)) {
 					continue;
 				}
-				else if (i%2 != 0 && i%2 != 0) {
+				else if (i%2 != 0 && j%2 != 0) {
 					laberintoa[i][j].setBlokeGogorra();
 				}
 				else {
