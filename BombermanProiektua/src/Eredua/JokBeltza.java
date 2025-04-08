c3631fcfdb71ecb07e16c2d8b8317f0549be7c7d
anderibarraa <aibarra039@ikasle.ehu.eus>
2025-04-08 18:06:24 +0200

    jokBeltza ultrabomb jarri beti

--- BombermanProiektua/src/Eredua/JokBeltza.java
+++ BombermanProiektua/src/Eredua/JokBeltza.java
@@ -1,24 +1,15 @@
 package Eredua;
 
 public class JokBeltza extends Jokalaria {
-	private int UltraBombaKont;
 
 	public JokBeltza(int pBombaKont, int pI, int pJ) {
 		super(pBombaKont, pI, pJ);
-		this.UltraBombaKont = 1;
 	}
 
 	@Override
 	public void bombaJarri() {
-		if (this.UltraBombaKont > 0) {
+		if (super.bombaKont>0) {
 			Laberintoa.getNireLaberintoa().bombaJarri(i, j, 2);
-			this.UltraBombaKont--;
-			if (this.UltraBombaKont == 0) {
-				super.timerBomba();
-			}
-		}
-		else if (super.bombaKont>0) {
-			Laberintoa.getNireLaberintoa().bombaJarri(i, j, 1);
 			super.bombaKont--;
 			if(super.bombaKont==0) {
 				super.timerBomba();
